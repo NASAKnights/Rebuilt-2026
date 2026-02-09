ebfae7804418fcaa447ca5b71d2db0b6748b6010
Thanco002 <codingminecrafter@gmail.com>
2026-02-09 18:09:34 -0500

    operator

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -253,27 +253,27 @@ void Robot::BindCommands()
     //         frc2::InstantCommand([this]
     //                              { return m_pathfind.Cancel(); })));
 
-    frc2::JoystickButton(&m_driverController, 1)
-                .WhileTrue(LaunchSequence(&m_fuelSubsystem).ToPtr())
-                .OnFalse(frc2::CommandPtr(
-                    frc2::InstantCommand([this]
-                    { return m_fuelSubsystem.stop(); })));
+    // frc2::JoystickButton(&m_driverController, 1)
+    //             .WhileTrue(LaunchSequence(&m_fuelSubsystem).ToPtr())
+    //             .OnFalse(frc2::CommandPtr(
+    //                 frc2::InstantCommand([this]
+    //                 { return m_fuelSubsystem.stop(); })));
 
-    frc2::JoystickButton(&m_driverController, 5)
-                .WhileTrue(frc2::CommandPtr(
-                    frc2::InstantCommand([this]
-                                { return m_fuelSubsystem.intake(); })))
-                .OnFalse(frc2::CommandPtr(
-                    frc2::InstantCommand([this]
-                    { return m_fuelSubsystem.stop(); })));
+    // frc2::JoystickButton(&m_driverController, 5)
+    //             .WhileTrue(frc2::CommandPtr(
+    //                 frc2::InstantCommand([this]
+    //                             { return m_fuelSubsystem.intake(); })))
+    //             .OnFalse(frc2::CommandPtr(
+    //                 frc2::InstantCommand([this]
+    //                 { return m_fuelSubsystem.stop(); })));
 
-    frc2::JoystickButton(&m_driverController, 6)
-                .WhileTrue(frc2::CommandPtr(
-                    frc2::InstantCommand([this]
-                                { return m_fuelSubsystem.eject(); })))
-                .OnFalse(frc2::CommandPtr(
-                    frc2::InstantCommand([this]
-                    { return m_fuelSubsystem.stop(); })));
+    // frc2::JoystickButton(&m_driverController, 6)
+    //             .WhileTrue(frc2::CommandPtr(
+    //                 frc2::InstantCommand([this]
+    //                             { return m_fuelSubsystem.eject(); })))
+    //             .OnFalse(frc2::CommandPtr(
+    //                 frc2::InstantCommand([this]
+    //                 { return m_fuelSubsystem.stop(); })));
 
 
     // --------------OPERATOR BUTTONS--------------------------------
@@ -287,29 +287,27 @@ void Robot::BindCommands()
     //         })));frc2::JoystickButton(&m_driverController, 1)
 
 
-//             frc2::JoystickButton(&m_operatorController, 1)
-//                 .OnTrue(frc2::CommandPtr(
-//                     frc2::InstantCommand([this]
-//                                 { return m_fuelSubsystem->launch(); })))
-//                 .OnFalse(frc2::CommandPtr(
-//                     frc2::InstantCommand([this]
-//                     { return m_fuelSubsystem->stop(); })));
+    frc2::JoystickButton(&m_operatorController, 1)
+                .WhileTrue(LaunchSequence(&m_fuelSubsystem).ToPtr())
+                .OnFalse(frc2::CommandPtr(
+                    frc2::InstantCommand([this]
+                    { return m_fuelSubsystem.stop(); })));
 
-//       frc2::JoystickButton(&m_operatorController, 5)
-//                 .OnTrue(frc2::CommandPtr(
-//                     frc2::InstantCommand([this]
-//                                 { return m_fuelSubsystem->intake(); })))
-//                 .OnFalse(frc2::CommandPtr(
-//                     frc2::InstantCommand([this]
-//                     { return m_fuelSubsystem->stop(); })));
+    frc2::JoystickButton(&m_operatorController, 5)
+                .WhileTrue(frc2::CommandPtr(
+                    frc2::InstantCommand([this]
+                                { return m_fuelSubsystem.intake(); })))
+                .OnFalse(frc2::CommandPtr(
+                    frc2::InstantCommand([this]
+                    { return m_fuelSubsystem.stop(); })));
 
-//  frc2::JoystickButton(&m_operatorController, 6)
-//                 .OnTrue(frc2::CommandPtr(
-//                     frc2::InstantCommand([this]
-//                                 { return m_fuelSubsystem->eject(); })))
-//                 .OnFalse(frc2::CommandPtr(
-//                     frc2::InstantCommand([this]
-//                     { return m_fuelSubsystem->stop(); })));
+    frc2::JoystickButton(&m_operatorController, 6)
+                .WhileTrue(frc2::CommandPtr(
+                    frc2::InstantCommand([this]
+                                { return m_fuelSubsystem.eject(); })))
+                .OnFalse(frc2::CommandPtr(
+                    frc2::InstantCommand([this]
+                    { return m_fuelSubsystem.stop(); })));
 }
 
 void Robot::DisabledPeriodic()
